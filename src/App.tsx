34f577db776b593c769ac589af55d0869470cfe3
vicGeo <vicsanig@alumni.upv.es>
2023-07-03 17:42:01 +0200

    add library tanstack react

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from '@tanstack/react-query';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const getRandomNumberFromApi = async (): Promise<number> => {
+  const res = await fetch(
+    'https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new'
+  );
+  const numberString = await res.text();
 
+  return +numberString;
+};
+
+export const App = () => {
+  const query = useQuery(['randomNumber'], getRandomNumberFromApi);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <main className='App App-header'>
+      {query.isFetching ? (
+        <h2>Cargando...</h2>
+      ) : (
+        <h2>Número aleatorio: {query.data} </h2>
+      )}
+      {!query.isLoading && query.isError && <h3>{`${query.error}`}</h3>}
+      <button onClick={() => query.refetch()} disabled={query.isFetching}>
+        {query.isFetching ? '...' : 'Nuevo número'}
+      </button>
+    </main>
+  );
+};
