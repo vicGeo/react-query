37f3fbf7bcfafa065364e1e4976eadf5ad4bd37a
vicGeo <vicsanig@alumni.upv.es>
2023-07-05 18:37:14 +0200

    add custom hook useRandom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
+import { useRandom } from './hooks/useRandom';
 import './App.css';
 
-const getRandomNumberFromApi = async (): Promise<number> => {
-  const res = await fetch(
-    'https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new'
-  );
-  const numberString = await res.text();
-
-  return +numberString;
-};
-
 export const App = () => {
-  const query = useQuery(['randomNumber'], getRandomNumberFromApi);
+  const query = useRandom();
+
   return (
     <main className='App App-header'>
       {query.isFetching ? (
